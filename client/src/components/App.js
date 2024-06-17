f2aea872a475e0b6109e9a87f32435dc0e0e0763
Fernando De Jesus <fdejesus95@outlook.com>
2024-06-17 12:39:34 -0400

    updated the react router dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react"; 
-import '../App.css' 
-import NavBar from "./Navbar" 
+// import '../App.css';
+import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App"> 
-      <NavBar />
+      <Navbar />
       <Outlet />
     </div>
   );
