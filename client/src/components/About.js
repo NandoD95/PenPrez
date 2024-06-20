6bc8bca3d465aacd5d3a09e33ccf89fca0ee250d
Fernando De Jesus <fdejesus95@outlook.com>
2024-06-20 16:38:32 -0400

    fixed spelling error

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -16,7 +16,7 @@ function About(){
                 <div className="image-text-wrapper">
                     <img src={vinJpeg} alt="Vinny" className="about-image" />
                     <div className="vinny-text">
-                        <h1>About Vinny</h1>
+                        <h1>About Vincent</h1>
                         <li><p>Throughout my four years at The University of Tampa, my passion for finance and relationship-building has been the driving force behind my achievements and personal growth. From the very beginning, I've been fueled by an unwavering desire to assist others in overcoming challenges and achieving their goals.</p></li>
                         <li><p>Penprez represents an exciting opportunity for me to continue pursuing my passion. At Penprez, I am empowered to help businesses scale and thrive by providing strategic financial insights and solutions. This role not only allows me to leverage my financial expertise but also enables me to cultivate and nurture lasting relationships with clients. I am deeply committed to contributing to their success, fostering trust, and building partnerships that stand the test of time.</p></li>
                         <li><p>My experience at The University of Tampa has equipped me with the skills and knowledge necessary to excel in this field. It has also reinforced my belief in the importance of helping others succeed. With Penprez, I am thrilled to be in a position where I can make a meaningful impact, guiding businesses towards growth and prosperity while forming enduring connections that benefit all parties involved.</p></li>
--- client/src/components/WorkWithUs.js
+++ client/src/components/WorkWithUs.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function WorkWithUs(){
     return(
         <div>
